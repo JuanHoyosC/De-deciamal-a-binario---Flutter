1f9da1b5909a5292a5dc75e32873d7c821a6f103
Juan Hoyos <jcabarique@uninorte.edu.co>
2020-11-03 12:00:00 -0500

    Se termino el cobvertidor de decimal a binario

--- lib/widgets/converter.dart
+++ lib/widgets/converter.dart
@@ -11,14 +11,14 @@ class _ConverterState extends State<Converter> {
 
   void _onPressed(dato) {
     setState(() {
-      _binary = _binary + dato;
+      _decimal = _decimal + dato;
       _convertir();
     });
   }
 
   void _convertir() {
     setState(() {
-      _decimal = int.parse(_binary, radix: 2).toRadixString(10);
+      _binary = int.parse(_decimal, radix: 10).toRadixString(2);
     });
   }
 
@@ -37,8 +37,19 @@ class _ConverterState extends State<Converter> {
           children: <Widget>[
             Container(
                 alignment: Alignment.centerLeft,
-                child: Text("Binary -> Decimal"),
+                child: Text("Decimal -> Binary"),
                 padding: const EdgeInsets.all(8.0)),
+            Container(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                '$_decimal',
+                textAlign: TextAlign.right,
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Color(int.parse("#ff8e2a".replaceAll('#', '0xff'))),
+                    fontSize: 35),
+              ),
+            ),
             Container(
                 padding: const EdgeInsets.all(8.0),
                 alignment: Alignment.centerRight,
@@ -52,18 +63,7 @@ class _ConverterState extends State<Converter> {
                       fontSize: 35),
                 )),
             Container(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                '$_decimal',
-                textAlign: TextAlign.right,
-                style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    color: Color(int.parse("#ff8e2a".replaceAll('#', '0xff'))),
-                    fontSize: 35),
-              ),
-            ),
-            Container(
-                height: 400,
+                height: 100,
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -88,6 +88,163 @@ class _ConverterState extends State<Converter> {
                       padding: const EdgeInsets.all(8.0),
                       child: MaterialButton(
                         color: Colors.blue,
+                        child: Text("2",
+                            style: new TextStyle(
+                              fontSize: 20.0,
+                              color: Colors.white,
+                            )),
+                        onPressed: () {
+                          _onPressed("2");
+                        },
+                      ),
+                    )),
+                    Expanded(
+                        child: Container(
+                      padding: const EdgeInsets.all(8.0),
+                      child: MaterialButton(
+                        color: Colors.blue,
+                        child: Text("3",
+                            style: new TextStyle(
+                              fontSize: 20.0,
+                              color: Colors.white,
+                            )),
+                        onPressed: () {
+                          _onPressed("3");
+                        },
+                      ),
+                    )),
+                  ],
+                )),
+            Container(
+                height: 100,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  mainAxisSize: MainAxisSize.max,
+                  children: <Widget>[
+                    Expanded(
+                        child: Container(
+                            padding: const EdgeInsets.all(8.0),
+                            child: MaterialButton(
+                              color: Colors.blue,
+                              child: Text("4",
+                                  style: new TextStyle(
+                                    fontSize: 20.0,
+                                    color: Colors.white,
+                                  )),
+                              onPressed: () {
+                                _onPressed("4");
+                              },
+                            ))),
+                    Expanded(
+                        child: Container(
+                      padding: const EdgeInsets.all(8.0),
+                      child: MaterialButton(
+                        color: Colors.blue,
+                        child: Text("5",
+                            style: new TextStyle(
+                              fontSize: 20.0,
+                              color: Colors.white,
+                            )),
+                        onPressed: () {
+                          _onPressed("5");
+                        },
+                      ),
+                    )),
+                    Expanded(
+                        child: Container(
+                      padding: const EdgeInsets.all(8.0),
+                      child: MaterialButton(
+                        color: Colors.blue,
+                        child: Text("6",
+                            style: new TextStyle(
+                              fontSize: 20.0,
+                              color: Colors.white,
+                            )),
+                        onPressed: () {
+                          _onPressed("6");
+                        },
+                      ),
+                    )),
+                  ],
+                )),
+            Container(
+                height: 100,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  mainAxisSize: MainAxisSize.max,
+                  children: <Widget>[
+                    Expanded(
+                        child: Container(
+                            padding: const EdgeInsets.all(8.0),
+                            child: MaterialButton(
+                              color: Colors.blue,
+                              child: Text("7",
+                                  style: new TextStyle(
+                                    fontSize: 20.0,
+                                    color: Colors.white,
+                                  )),
+                              onPressed: () {
+                                _onPressed("7");
+                              },
+                            ))),
+                    Expanded(
+                        child: Container(
+                      padding: const EdgeInsets.all(8.0),
+                      child: MaterialButton(
+                        color: Colors.blue,
+                        child: Text("8",
+                            style: new TextStyle(
+                              fontSize: 20.0,
+                              color: Colors.white,
+                            )),
+                        onPressed: () {
+                          _onPressed("8");
+                        },
+                      ),
+                    )),
+                    Expanded(
+                        child: Container(
+                      padding: const EdgeInsets.all(8.0),
+                      child: MaterialButton(
+                        color: Colors.blue,
+                        child: Text("9",
+                            style: new TextStyle(
+                              fontSize: 20.0,
+                              color: Colors.white,
+                            )),
+                        onPressed: () {
+                          _onPressed("9");
+                        },
+                      ),
+                    )),
+                  ],
+                )),
+            Container(
+                height: 100,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  mainAxisSize: MainAxisSize.max,
+                  children: <Widget>[
+                    Expanded(
+                        child: Container(
+                            padding: const EdgeInsets.all(8.0),
+                            child: MaterialButton(
+                              color: Colors.white,
+                              child: Text("",
+                                  style: new TextStyle(
+                                    fontSize: 20.0,
+                                    color: Colors.white,
+                                  )),
+                              onPressed: () {},
+                            ))),
+                    Expanded(
+                        child: Container(
+                      padding: const EdgeInsets.all(8.0),
+                      child: MaterialButton(
+                        color: Colors.blue,
                         child: Text("0",
                             style: new TextStyle(
                               fontSize: 20.0,
@@ -96,6 +253,19 @@ class _ConverterState extends State<Converter> {
                         onPressed: () {
                           _onPressed("0");
                         },
+                      ),
+                    )),
+                    Expanded(
+                        child: Container(
+                      padding: const EdgeInsets.all(8.0),
+                      child: MaterialButton(
+                        color: Colors.white,
+                        child: Text("",
+                            style: new TextStyle(
+                              fontSize: 20.0,
+                              color: Colors.transparent,
+                            )),
+                        onPressed: () {},
                       ),
                     )),
                   ],
